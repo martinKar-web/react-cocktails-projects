e8da118ddbaf35e2495af04aabf8fc6b468bc3d6
Martin Karugaba <martinkarugaba21@gmail.com>
2022-07-09 15:11:34 +0300

    updated react router and completed project

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+        aperiam eligendi ex, repellendus saepe a, quisquam iure neque
+        debitis culpa itaque illo quo nostrum rerum vitae nulla
+        dolorem! Deleniti optio animi enim id ipsam eaque consectetur
+        ex eum nihil vero.
+      </p>
+    </section>
+  );
+};
+
+export default About;
